b3e57812cc344e027c46307c993bfdc4124e2c2d
will9rmx <guillermo.leon@doofinder.com>
2025-02-18 22:36:12 +0100

    Create doofinder.js

---
+++ doofinder.js
@@ -0,0 +1,5 @@
+// Doofinder script injection
+var script = document.createElement('script');
+script.src = 'https://us1-config.doofinder.com/2.x/bb3971cc-2ea3-4d23-a621-82ad5f82faa7.js';
+script.async = true;
+document.head.appendChild(script);
